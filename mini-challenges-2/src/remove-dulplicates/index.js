db245287d7b2b5b5eeaefb390c4033c69a8b8680
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-08-03 01:07:17 +0100

    added remove duplicate solution

--- mini-challenges-2/src/remove-dulplicates/index.js
+++ mini-challenges-2/src/remove-dulplicates/index.js
@@ -1,25 +1,3 @@
-function removeDuplicates(obj) {
-    const allKeys = Object.keys(obj).reverse();
-    const row = allKeys.length;
-    let newAllValues = [];
-    let checker = "";
-    let result = {};
-    for (let i = 0; i < row; i++) {
-        let times = obj[allKeys[i]].length;
-        let tempArray = []; //Temporary Array for each iteration
-        for (let j = 0; j < times; j++) {
-            let currentLetter = obj[allKeys[i]][j]; //Current Letter to be checked
-            if (checker.includes(currentLetter)) { //If it is in the checker, do nothing
-                continue
-            } else {
-                tempArray.push(currentLetter); // If not in checker push to tempArray and add to checker
-                checker += currentLetter;
-            }
-            newAllValues.push(tempArray); //After the inner iteration push to array of new values
-        }
-        result[allKeys[i]] = tempArray; //Create new value for the key in the result array
-    }
-    return result
-}
+function removeDuplicates(obj) {}
 
 module.exports = removeDuplicates;
